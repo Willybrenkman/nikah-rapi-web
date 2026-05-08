44690c6e11765ae8233646d3fb9556e10feadc33
Willybrenkman <willybrenkman@binus.ac.id>
2026-05-09 03:02:06 +0700

    fix: import useEffect in Demo.jsx

--- src/pages/Demo.jsx
+++ src/pages/Demo.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Chart as ChartJS, ArcElement, CategoryScale, LinearScale, BarElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut, Bar, Pie } from 'react-chartjs-2';
